9fc1e50525a1fe93fd2e7ed630c7f1281d51ac34
Apostolos Kapetanios <ap.kapetanios@gmail.com>
2022-01-29 23:47:56 +0200

    Seems OK

--- Backend/index.js
+++ Backend/index.js
@@ -1,154 +1,24 @@
 const express = require("express");
 const app = express();
-const pool = require("./services/dbService");
+const taxonomy = require("./routes/taxonomy");
 
-app.use(express.json());
 const cors = require("cors");
 const { filter } = require("lodash");
+
+app.use(express.json());
+
+app.use(function (error, req, res, next) {
+  if (error instanceof SyntaxError) {
+    //Handle SyntaxError here.
+    return res.status(500).send({ data: "Invalid data" });
+  } else {
+    next();
+  }
+});
+
 app.use(cors({ origin: "*" }));
 
-app.get("/", async (req, res) => {
-  let sql = `SELECT 
-  n.tax_id,
-  n.parent_tax_id,
-  n.rank,
-  a.name_txt
-  From nodes as n
-  INNER JOIN tax_names as a
-  ON a.tax_id=n.tax_id
-  WHERE a.name_class= 'scientific name' limit 50`;
-  pool.query(sql, (error, results, fields) => {
-    if (error) {
-      return console.error(error.message);
-    }
-    res.send(results);
-  });
-});
-
-app.post("/filter_data", async (req, res) => {
-  // [
-  //   { field: [ 'tax_id', 'abc' ] },
-  //   { boolean: 'or', field: [ 'name_txt', 'def' ] }
-  // ]
-  const filter = req.body;
-  let sqlPredicate = "";
-  console.log(req.body);
-  if (Array.isArray(filter)) {
-    numOfItems = filter.length;
-    count = 0;
-    filter.forEach((item, index) => {
-      count++;
-      let boolean = "";
-      let field = item["field"];
-      let column = field[0];
-      let value = field[1];
-
-      if (item["boolean"] !== undefined) {
-        boolean = item["boolean"];
-      }
-
-      if (column === "tax_id") {
-        spec_key = "n.tax_id";
-      } else {
-        spec_key = column;
-      }
-
-      if (!boolean) {
-        sqlPredicate += `${spec_key} like '${value}%' `;
-      } else {
-        sqlPredicate += `${boolean} ${spec_key} like '${value}%'`;
-      }
-
-      console.log(sqlPredicate);
-    });
-  }
-  // console.log(sqlPredicate);
-  let sql = `SELECT
-  n.tax_id,
-  n.parent_tax_id,
-  n.rank,
-  a.name_txt
-  From nodes as n
-  INNER JOIN tax_names as a
-  ON a.tax_id=n.tax_id
-  WHERE a.name_class= 'scientific name' AND ( ${sqlPredicate} )`;
-  console.log(sql);
-  pool.query(sql, (error, results, fields) => {
-    if (error) {
-      return console.error(error.message);
-    }
-    res.send(results);
-  });
-});
-
-app.get("/tax_id/:id", (req, res) => {
-  let id = req.params.id;
-  // let sql = `SELECT * FROM nodes where tax_id = ${id}`;
-  let sql = `
-  SELECT 
-n.tax_id,
-n.parent_tax_id,
-n.rank,
-a.name_txt
-From nodes as n
-INNER JOIN tax_names as a
-ON a.tax_id=n.tax_id
-WHERE a.name_class= 'scientific name'
-AND n.tax_id = ${id};`;
-
-  pool.query(sql, (error, results, fields) => {
-    if (error) {
-      return console.error(error.message);
-    }
-    res.send(results);
-  });
-});
-
-app.get("/taxonomy_taxid/:id", (req, res) => {
-  let id = req.params.id;
-  // let sql = `SELECT * FROM nodes where tax_id = ${id}`;
-  let sql = `SELECT * From nodes 
-  INNER JOIN tax_names 
-  ON tax_names.tax_id=nodes.tax_id 
-  INNER JOIN gencode
-  ON gencode.genetic_code_id=nodes.tax_id WHERE tax_names.tax_id = ${id} and name_class = 'scientific name'; `;
-
-  pool.query(sql, (error, results, fields) => {
-    if (error) {
-      return console.error(error.message);
-    }
-    res.send(results);
-  });
-});
-
-app.get("/taxonomy_parent/:id", (req, res) => {
-  let id = req.params.id;
-  let sql = `SELECT * FROM nodes where parent_tax_id = ${id}`;
-
-  pool.query(sql, (error, results, fields) => {
-    if (error) {
-      return console.error(error.message);
-    }
-    res.send(results);
-  });
-});
-
-app.post("/tax_names/search_unique_name", (req, res) => {
-  let unique_name = req.body.unique_name.toLowerCase();
-
-  let sql = `SELECT * 
-              FROM tax_names 
-              where LOWER(unique_name) 
-              like '${unique_name}%' 
-              and name_class <> 'type material';`;
-
-  pool.query(sql, (error, results, fields) => {
-    if (error) {
-      return console.error(error.message);
-    }
-    res.send(results);
-  });
-});
+app.use("/api", taxonomy);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- FrontEnd/src/components/Taxonomy_taxid.jsx
+++ FrontEnd/src/components/Taxonomy_taxid.jsx
@@ -2,11 +2,13 @@ import { Component } from "react";
 import httpService from "../services/httpService";
 import { Link } from "react-router-dom";
 import ClipBoardIcon from "./common/ClipBoardIcon";
+import _ from "lodash";
 
 class Taxonomy_taxid extends Component {
   state = {
     myData: [],
     showModal: false,
+    scientificName: "",
   };
 
   showModal = () => {
@@ -20,10 +22,12 @@ class Taxonomy_taxid extends Component {
   async componentDidMount() {
     let id = this.props.match.params.id;
     const { data } = await httpService.get(
-      `http://localhost:3001/taxonomy_taxid/${id}`
+      `http://localhost:3001/api/taxonomy_taxid/${id}`
     );
-    console.log(data);
-    this.setState({ myData: data });
+    this.setState({ myData: data }, () => {
+      const objectFound = _.find(data, { name_class: "scientific name" });
+      this.setState({ scientificName: objectFound.name_txt });
+    });
   }
 
   render() {
@@ -31,23 +35,24 @@ class Taxonomy_taxid extends Component {
       <div className="row" style={{ marginTop: "20px" }}>
         <div className="col-lg-1 col-sm-1"></div>
         <div className="col-lg-10 col-sm-10">
+          <h4>Scientific Name: {this.state.scientificName}</h4>
           <table className="table">
             <thead>
               <tr>
                 <th scope="col">Tax_ID</th>
                 <th scope="col">Rank</th>
                 <th scope="col">Embl_Code</th>
-                <th scope="col">TAX_Name</th>
+                <th scope="col">Name/Synonyms</th>
                 <th scope="col">Genetic_code_id</th>
-                <th scope="col">mitochondrial_genetic_code_id</th>
                 <th scope="col">Parent Tax_ID</th>
+                <th scope="col">Name Class</th>
               </tr>
             </thead>
             <tbody>
               {this.state.myData.map((row) => (
                 <tr key={row.tax_id}>
                   <td>{row.tax_id}</td>
-                  <td>{row.rank}</td>
+                  <td>{row.rank_id}</td>
                   <td>{row.embl_code}</td>
                   <td>{row.name_txt}</td>
                   <td>
@@ -71,6 +76,7 @@ class Taxonomy_taxid extends Component {
                       {row.parent_tax_id}
                     </Link>
                   </td>
+                  <td>{row.name_class}</td>
                 </tr>
               ))}
             </tbody>
